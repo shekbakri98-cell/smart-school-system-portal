3dcb72e77978d20fe251e6f3811887087a5951bd
shekbakri98-cell <shekbakri98@gmail.com>
2026-08-31 23:26:45 +0300

    Update route.js

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { connectToDatabase } from '@/lib/db';
+import { connectToDatabase } from '../../../lib/db'; // Fixed relative path import style
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
